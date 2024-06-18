caa2778ae959fd6ecba282eb0b4e013ffdbafde7
ShruthiVallapReddy <122497616+ShruthiVallapReddy@users.noreply.github.com>
2024-06-17 23:37:40 -0500

    1st commit
    commit

---
+++ helloworld/src/main/java/com/javacode/helloworld/HelloController.java
@@ -0,0 +1,13 @@
+package com.javacode.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World application created from java";
+    }
+
+}
